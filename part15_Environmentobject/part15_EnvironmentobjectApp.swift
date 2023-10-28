0863e0ff9fdec57bc513148b9daf7a0f5c5cd1d6
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-28 15:57:48 +0700

    Initial Commit

---
+++ part15_Environmentobject/part15_EnvironmentobjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  part15_EnvironmentobjectApp.swift
+//  part15_Environmentobject
+//
+//  Created by emi oiso on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct part15_EnvironmentobjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
